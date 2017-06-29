73da8dd66e466ac011248cb7b52427b269435a15
Szalay-Nagy Bálint <szalaynb93@gmail.com>
2017-06-29 17:05:32 +0200

    done

--- export.py
+++ export.py
@@ -1,2 +1,7 @@
+from report import *
 
-# Export functions
+print count_games(file_name)
+print decide(file_name, year)
+print get_latest(file_name)
+print count_by_genre(file_name, genre)
+print get_line_number_by_title(file_name, title)
--- printing.py
+++ printing.py
@@ -1,2 +1,7 @@
-
-# Printing functions
+from reports import *
+file_name = "game_stat.txt"
+print count_games(file_name)
+print decide(file_name, 1999)
+print get_latest(file_name)
+print count_by_genre(file_name, "RPG")
+print get_line_number_by_title(file_name, "Counter-Strike")
